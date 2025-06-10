8fe00b636cf3379df1ecf3b3d11a0a3f3eb61eb5
MluisaG <luisgalazmp@gmail.com>
2025-06-10 09:46:44 -0700

    Listo el proyecto de lista de compras

--- class_2/practica/src/components/ListaCompras.jsx
+++ class_2/practica/src/components/ListaCompras.jsx
@@ -1,25 +1,43 @@
 import { useState } from "react";
-
+import './ListaCompras.css';
 
 function ListaCompras(){
-    const [productos, setProductos] = useState();
+    const [productos, setProductos] = useState([]);
     const[nuevoProducto, setNuevoProducto]=useState();
 
+     // Función para agregar un nuevo producto a la lista
     const agregarProducto = ()=>{
         if(nuevoProducto.trim() !== ''){
             setProductos([...productos,nuevoProducto]);
             setNuevoProducto('');
         }
     }
+    
     const eliminarProducto = (index)=>{
-        setProductos(productos.splice(index,1))
+        setProductos(productos.filter((valor, indice)=>indice != index));
+        // productos.splice(index,1);
+        // setProductos([...productos])
     }
 
     return (
         <div>
             <h2>Lista de Compras</h2>
-            <input type="text" />
+            <section className="form">
+                <input type="text" value = {nuevoProducto} onChange={(e)=>setNuevoProducto(e.target.value)}/>
+                <button onClick={agregarProducto}>Agregar</button>
+            </section>
+            <section>
+                <ul>
+                    {productos.map((producto, index)=>(
+                        <li key={index}>
+                            {producto}
+                            <button onClick={()=>eliminarProducto(index)}>Eliminar</button>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </div>
+
     )
 }
 
