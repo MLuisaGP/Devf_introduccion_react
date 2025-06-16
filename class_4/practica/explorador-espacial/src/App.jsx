da406b97909e8134cfba8cb9ebe38c416826d52b
MluisaG <luisgalazmp@gmail.com>
2025-06-16 10:01:50 -0700

    avance

--- class_4/practica/explorador-espacial/src/App.jsx
+++ class_4/practica/explorador-espacial/src/App.jsx
@@ -3,23 +3,32 @@ import './App.css'
 import Planeta from './componentes/Planeta';
 
 function App() {
-  // ... (estado)
-  const[estadoNave,setEstado]=useState('en Orbita');
-  const[distancia,setDistancia]=useState(1);
-  const[planetasVisitados,setPlaneta]=useState(['Marte','Jupiter']);
+  
+  const [distancia,setDistancia]=useState(0);
+  const [combustible, setCombustible]= useState(100);
+  const [estadoNave,setEstado]=useState('En Orbita');
+  const [planetasVisitados,setPlanetaVisitado]=useState([]);
+  const [newPlaneta,setNewPlaneta]=useState('Mercurio');
 
   useEffect(() => {
     console.log("¡El panel está listo!"); // Montaje
 
-    const intervalo = setInterval(() => { // Montaje
-      // ... (simulación de vuelo)
+    
+    const  intervalo= setInterval(() => { // Montaje
+      explorando()
     }, 1000);
-
     return () => {
       clearInterval(intervalo); // Desmontaje
       console.log("El panel se ha apagado."); // Desmontaje
     };
-  }, []);
+  }, [combustible,distancia]);
+
+  const explorando = ()=>{
+    if(estadoNave == 'En Orbita'){
+      setCombustible(combustible-1);
+      setDistancia(distancia+1);
+    }
+  }
 
   useEffect(() => {
     console.log("¡Combustible actualizado!"); // Actualización
@@ -29,8 +38,16 @@ function App() {
     return `Estado: ${estadoNave}`;
   }, [estadoNave]);
 
+  const aterizaje = ()=>{
+    setEstado('Aterrizando');
+    setPlanetaVisitado([...planetasVisitados,newPlaneta])
+  }
   return (
     <div>
+        <p>Distancia: {distancia}</p>
+        <p>Combustible: {combustible}</p>
+        <p>{mensajeEstado}</p>
+        <button onClick={aterizaje}>Aterrizar</button>
       {/* ... (información del panel) */}
       {planetasVisitados.map((planeta, index) => (
         <Planeta key={index} nombre={planeta} />
