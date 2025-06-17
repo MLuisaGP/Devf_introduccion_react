bd3242be5f0135256aa6eb3b97c4dfcadfe7c28c
MluisaG <luisgalazmp@gmail.com>
2025-06-17 08:55:12 -0700

    avances con planeta

--- class_4/practica/explorador-espacial/src/App.jsx
+++ class_4/practica/explorador-espacial/src/App.jsx
@@ -1,59 +1,65 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Planeta from './componentes/Planeta';
+import { MemoEstado } from './hooks/MemoEstado';
 
 function App() {
-  
-  const [distancia,setDistancia]=useState(0);
-  const [combustible, setCombustible]= useState(100);
-  const [estadoNave,setEstado]=useState('En Orbita');
-  const [planetasVisitados,setPlanetaVisitado]=useState([]);
-  const [newPlaneta,setNewPlaneta]=useState('Mercurio');
+
+  const [distancia, setDistancia] = useState(0);
+  const [combustible, setCombustible] = useState(100);
+  const [estadoNave, setEstado] = useState('En Planeta');
+  const [avanzando, setAvanzando] = useState(false);
+
 
   useEffect(() => {
     console.log("¡El panel está listo!"); // Montaje
+    const intervalo = setInterval(() => { // Montaje
+      if (combustible <= 0) {
+        setAvanzando(false)
+      } else {
+        explorando()
+      }
 
-    
-    const  intervalo= setInterval(() => { // Montaje
-      explorando()
     }, 1000);
     return () => {
       clearInterval(intervalo); // Desmontaje
       console.log("El panel se ha apagado."); // Desmontaje
     };
-  }, [combustible,distancia]);
+  }, [combustible, distancia, avanzando]);
 
-  const explorando = ()=>{
-    if(estadoNave == 'En Orbita'){
-      setCombustible(combustible-1);
-      setDistancia(distancia+1);
+  const explorando = () => {
+    if (avanzando && combustible > 0) {
+      setCombustible(combustible - 1);
+      setDistancia(distancia + 1);
     }
   }
 
-  useEffect(() => {
-    console.log("¡Combustible actualizado!"); // Actualización
-  }, [combustible]);
+  useEffect(
+    () => {
+      setEstado(`${avanzando ? 'Despegando' : 'Aterizando'}...`);
+      const interval = setTimeout(() => {
+        setEstado(`En ${avanzando ? 'Orbita' : 'Planeta'}`);
+      }, 2000)
+      return () => {
+        clearInterval(interval); // Desmontaje
+      };
+    }, [avanzando])
 
-  const mensajeEstado = useMemo(() => {
-    return `Estado: ${estadoNave}`;
-  }, [estadoNave]);
 
-  const aterizaje = ()=>{
-    setEstado('Aterrizando');
-    setPlanetaVisitado([...planetasVisitados,newPlaneta])
-  }
+
   return (
     <div>
-        <p>Distancia: {distancia}</p>
-        <p>Combustible: {combustible}</p>
-        <p>{mensajeEstado}</p>
-        <button onClick={aterizaje}>Aterrizar</button>
-      {/* ... (información del panel) */}
-      {planetasVisitados.map((planeta, index) => (
-        <Planeta key={index} nombre={planeta} />
-      ))}
+      <p>Distancia: {distancia}</p>
+      <p>Combustible: {combustible}</p>
+      <p>{MemoEstado(estadoNave)}</p>
+      <button onClick={() => {
+        setAvanzando(!avanzando)
+      }} disabled={combustible <= 0 && !avanzando}>{avanzando ? 'Aterrizar' : 'Despegar'}
+      </button>
+
+      {!avanzando && (<button>Registrar Planeta Nuevo</button>)}
     </div>
   );
 }
+
 
 export default App
