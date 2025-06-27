0516c84a3299088c2636af1d73d9ae22ce994579
LuisaGa96 <luisagalazmp@gmail.com>
2025-06-26 18:39:50 -0700

    Listo clase 6

--- class_6/notas_clase/useRedux/src/App.jsx
+++ class_6/notas_clase/useRedux/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer } from 'react'
 import './App.css'
 
+// Hacer 3 botones para incrementar, decrementar y otro de reiniciar.
+
+const initialState = {count:0,activo:true}
+
+function reduce(state,action){
+  switch (action.type) {
+    case "incrementar":
+      return { count: state.count + 1, activo: state.activo };
+    case "decrementar":
+      return { count: state.count - 1, activo: state.activo };
+    case "reiniciar":
+      return { count: 0, activo: state.activo };
+    case "desactivar":
+      return { count: state.count, activo: !state.activo };
+    default:
+      throw new Error("Accion no reconocida");
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  //Desestructuracion de reduce
+  const [state,dispatch] = useReducer(reduce,initialState);//dispatch es para enviar el estado pero hay que convertirlo en reduce ya que revisa que accion estamos haciendo
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Contador: {state.count}</h2>
+      <button onClick={() => dispatch({ type: "incrementar" })}>
+        Incrementar
+      </button>
+      <button onClick={() => dispatch({ type: "decrementar" })}>
+        Decrementar
+      </button>
+      <button onClick={() => dispatch({ type: "reiniciar" })}>Reiniciar</button>
+      <button onClick={() => dispatch({ type: "desactivar" })}>
+        {state.activo?'activo':'desactivo'}
+      </button>
     </>
-  )
+  );
 }
 
 export default App
