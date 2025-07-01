9642dd1de0fc70f508353890832f56fecc94c3b0
MluisaG <luisgalazmp@gmail.com>
2025-07-01 16:51:07 -0700

    se ingresa el login

--- class_8/mi_twitter_clone/src/App.jsx
+++ class_8/mi_twitter_clone/src/App.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import ProtectRoute from './components/ProtectRoute';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+       <Route
+        path="/profile"
+        element={
+          <ProtectRoute>
+            <Profile />
+          </ProtectRoute>
+        }
+      />
     </Routes>
   );
 }
