a6b2a02fb13a9ee6a848e47c6e22ed99de192fe1
LuisaGa96 <luisagalazmp@gmail.com>
2025-06-30 18:54:11 -0700

    Ya se muestran las citas

--- class_7/proyecto/citas-medicas/src/pages/CitaDetalle.jsx
+++ class_7/proyecto/citas-medicas/src/pages/CitaDetalle.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import Header from '../components/Header';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function CitaDetalle() {
   const { id } = useParams();
+  const data = JSON.parse(localStorage.getItem("citas"));
+  const cita = data.find((elemento) => elemento.id == id);
+  console.log(cita);
+
   return (
-    <div>
+    <div className="body">
       <Header />
-      <h1>Detalles Citas</h1>
-      <p>Id de la cita: {id}</p>
+      <section>
+        <h1>Detalles Citas</h1>
+        <p>Id de la cita: {cita.id}</p>
+        <p>Nombre paciente: {cita.paciente}</p>
+        <p>Doctor: {cita.doctor}</p>
+        <p>Dia: {cita.dia}</p>
+        <p>Hora: {cita.hora}</p>
+      </section>
     </div>
   );
 }
